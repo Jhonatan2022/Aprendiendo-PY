6f7374f650289031f1189ed3ce65380f18ce6d95
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2022-10-15 19:18:27 +0100

    no funcional

--- Modulo-2-python/primer modulo python/escrita.py
+++
@@ -1,9 +0,0 @@
-# Se importa hashlib
-import hashlib
-
-
-def encriptar(contraseña):
-
-    contraseña_cifrada = hashlib.sha512(contraseña.encode()) 
-
-    print("Su contraseña cifrada en hexadecimales es: " + contraseña_cifrada.hexdigest())
