1f3802f92498998cc57c1ed706ad31b6462d9d5c
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2022-10-15 19:17:49 +0100

    contraseña escrita y cifrada funcional

---
+++ Modulo-2-python/primer modulo python/main.py
@@ -0,0 +1,14 @@
+import hashlib
+
+from modulo import encriptar
+
+x= str (input("Dijite la dirección de su archivo: "))
+
+encriptar(x)
+
+
+from escrita import encriptar
+
+contraseña = input("Contraseña: ")
+
+encriptar(contraseña)
