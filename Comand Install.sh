c0e4da27a4bc595bae53f9e50d3a3d736df9234e
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-04-15 15:53:22 +0100

    Documentation of the code

---
+++ Comand Install.sh
@@ -0,0 +1,11 @@
+# Comand generate requirements.sh
+
+pip freeze > requirements.sh
+# or pip freeze > requirements.txt
+
+
+# Comand Install dependences
+# Install dependences
+
+pip install -r requirements.sh
+# or pip install -r requirements.txt
